250e4bac5e3ea17f8ad1cb92cb56e5a4677fa4f5
Xin <1498973064@qq.com>
2022-06-15 17:05:48 +0800

    third change

--- src/main/java/com/laoliu/MyApplication.java
+++ src/main/java/com/laoliu/MyApplication.java
@@ -5,5 +5,6 @@ public class MyApplication {
     public static void main(String[] args) {
         System.out.println("first version1");
         System.out.println("second version1");
+        System.out.println("third version1");
     }
 }
