4ac5234ad4af5585a2876096d4ace218e79c30b9
Xin <1498973064@qq.com>
2022-06-16 09:16:47 +0800

    test change

--- src/main/java/com/laoliu/MyApplication.java
+++ src/main/java/com/laoliu/MyApplication.java
@@ -5,7 +5,7 @@ public class MyApplication {
     public static void main(String[] args) {
         System.out.println("first version1");
         System.out.println("second version1");
-        System.out.println("third version1");
+        int a =  3;
         System.out.println("forth version1");
     }
 }
